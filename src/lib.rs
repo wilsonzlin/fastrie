76b8869b299fb694c50a5a80b37daeafb3685d73
Wilson Lin <code@wilsonl.in>
2022-11-12 17:14:06 +1100

    Allow custom index widths

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,71 @@
 use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests;
-
 pub struct FastrieBuilderNode<V> {
     built: bool,
-    value: Option<V>,
     children: HashMap<u8, FastrieBuilderNode<V>>,
+    index_width: IndexWidth,
+    value: Option<V>,
 }
 
-const IDX_BYTES: usize = 3;
 const RESERVED_BYTE: u8 = 0xFF;
 const MAX_CLUSTER_GAP_LEN: i16 = 3;
 
-fn reserve_idx(vec: &mut Vec<u8>) -> usize {
-    let pos = vec.len();
-    for _ in 0..IDX_BYTES {
-        vec.push(RESERVED_BYTE);
+/// How many bytes to store and represent indices in the built data. Must be between 1 and 8 inclusive. Indices will be encoded in little endian format.
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct IndexWidth(pub usize);
+
+impl IndexWidth {
+  fn reserve_idx(self, vec: &mut Vec<u8>) -> usize {
+      let pos = vec.len();
+      for _ in 0..self.0 {
+          vec.push(RESERVED_BYTE);
+      };
+      pos
+  }
+
+  fn write_idx(self, vec: &mut Vec<u8>, pos: usize, mut idx: usize) -> () {
+    for i in 0..self.0 {
+      vec[pos + i] = idx as u8;
+      idx >>= 8;
     };
-    pos
-}
+  }
 
-fn encode_idx(idx: usize) -> [u8; IDX_BYTES] {
-    assert!(idx <= 2usize.pow((8 * IDX_BYTES) as u32) - 1);
-    [(idx >> 16) as u8, (idx >> 8) as u8, idx as u8]
-}
+  fn push_idx(self, vec: &mut Vec<u8>, idx: usize) -> () {
+      let pos = self.reserve_idx(vec);
+      self.write_idx(vec, pos, idx);
+  }
 
-fn decode_idx(encoded: &[u8]) -> usize {
-    assert_eq!(encoded.len(), IDX_BYTES);
-    ((encoded[0] as usize) << 16) | ((encoded[1] as usize) << 8) | (encoded[2] as usize)
-}
-
-fn write_idx(vec: &mut Vec<u8>, pos: usize, idx: usize) -> () {
-    vec[pos..pos + IDX_BYTES].copy_from_slice(&encode_idx(idx))
-}
-
-fn push_idx(vec: &mut Vec<u8>, idx: usize) -> () {
-    vec.extend_from_slice(&encode_idx(idx))
-}
-
-fn read_idx(data: &[u8], pos: usize) -> usize {
-    decode_idx(&data[pos..pos + IDX_BYTES])
+  fn read_idx(self, data: &[u8], pos: usize) -> usize {
+      let mut idx = 0usize;
+      for i in 0..self.0 {
+        idx <<= 8;
+        idx |= data[pos + i] as usize;
+      }
+      idx
+  }
 }
 
 pub struct FastrieBuild<V> {
-    pub values: Vec<V>,
     pub data: Vec<u8>,
+    pub index_width: IndexWidth,
+    pub values: Vec<V>,
 }
 
 impl<V> FastrieBuilderNode<V> {
-    pub fn new() -> FastrieBuilderNode<V> {
-        FastrieBuilderNode { built: false, value: None, children: HashMap::new() }
+    pub fn new(index_width: IndexWidth) -> FastrieBuilderNode<V> {
+        FastrieBuilderNode {
+          built: false,
+          children: HashMap::new(),
+          index_width,
+          value: None,
+        }
     }
 
     pub fn add(&mut self, pattern: &[u8], value: V) -> () {
         let mut current: &mut FastrieBuilderNode<V> = self;
         for c in pattern {
             if !current.children.contains_key(c) {
-                current.children.insert(*c, FastrieBuilderNode::new());
+                current.children.insert(*c, FastrieBuilderNode::new(current.index_width));
             };
             current = current.children.get_mut(c).unwrap();
         };
@@ -74,7 +82,7 @@ impl<V> FastrieBuilderNode<V> {
             values.push(self.value.take().unwrap());
             idx
         } else { 0 };
-        push_idx(data, value_idx);
+        self.index_width.push_idx(data, value_idx);
 
         let mut child_chars = self.children.keys().map(|k| *k).collect::<Vec<u8>>();
         child_chars.sort();
@@ -110,9 +118,9 @@ impl<V> FastrieBuilderNode<V> {
         for cluster in &child_char_clusters {
             let cluster_pos = data.len();
             if let Some(out_pos) = last_cluster_next_cluster_dist_pos {
-                write_idx(data, out_pos, cluster_pos);
+                self.index_width.write_idx(data, out_pos, cluster_pos);
             };
-            last_cluster_next_cluster_dist_pos = Some(reserve_idx(data));
+            last_cluster_next_cluster_dist_pos = Some(self.index_width.reserve_idx(data));
             let min = cluster.first().unwrap().unwrap();
             let max = cluster.last().unwrap().unwrap();
             data.push(min);
@@ -121,20 +129,20 @@ impl<V> FastrieBuilderNode<V> {
                 match c {
                     Some(c) => {
                         debug_assert!(!replace_with_child_indices.contains_key(c));
-                        replace_with_child_indices.insert(*c, reserve_idx(data));
+                        replace_with_child_indices.insert(*c, self.index_width.reserve_idx(data));
                     }
-                    None => { push_idx(data, 0); }
+                    None => { self.index_width.push_idx(data, 0); }
                 };
             };
         };
         if let Some(out_pos) = last_cluster_next_cluster_dist_pos {
-            write_idx(data, out_pos, 0);
+            self.index_width.write_idx(data, out_pos, 0);
         };
 
         for cluster in &child_char_clusters {
             for c in cluster {
                 if let Some(c) = c {
-                    write_idx(data, *replace_with_child_indices.get(c).unwrap(), data.len());
+                    self.index_width.write_idx(data, *replace_with_child_indices.get(c).unwrap(), data.len());
                     let child_node = self.children.get_mut(c).unwrap();
                     child_node._build(data, values);
                 };
@@ -146,14 +154,19 @@ impl<V> FastrieBuilderNode<V> {
         let mut data: Vec<u8> = Vec::new();
         let mut values: Vec<V> = Vec::new();
         self._build(&mut data, &mut values);
-        FastrieBuild { values, data }
+        FastrieBuild {
+          data,
+          index_width: self.index_width,
+          values,
+        }
     }
 }
 
 pub struct Fastrie<'v, 'd, V> {
+    data: &'d [u8],
+    index_width: IndexWidth,
     // If None, keys are used as a set.
     values: Option<&'v [V]>,
-    data: &'d [u8],
 }
 
 pub struct FastrieMatch<'v, V> {
@@ -168,7 +181,7 @@ pub struct FastrieMatch<'v, V> {
 /// ```
 /// use fastrie::*;
 ///
-/// let mut builder = FastrieBuilderNode::new();
+/// let mut builder = FastrieBuilderNode::new(IndexWidth(1));
 /// builder.add(b"hell", 1);
 /// builder.add(b"hello", 2);
 /// builder.add(b"world", 4);
@@ -176,15 +189,19 @@ pub struct FastrieMatch<'v, V> {
 ///
 /// // `build.data` can be written as bytes to a file, or embedded directly into code as a literal byte array/slice.
 ///
-/// let trie = from_prebuilt_without_values(&build.data);
+/// let trie = from_prebuilt_without_values(build.index_width, &build.data);
 /// assert!(trie.contains_key(b"hell"));
 /// assert!(trie.contains_key(b"hello"));
 /// assert!(trie.contains_key(b"world"));
 /// assert!(!trie.contains_key(b"worl"));
 /// assert!(!trie.contains_key(b"worlds"));
 /// ```
-pub const fn from_prebuilt_without_values<'d>(data: &'d [u8]) -> Fastrie<'_, 'd, ()> {
-  Fastrie { values: None, data }
+pub const fn from_prebuilt_without_values<'d>(index_width: IndexWidth, data: &'d [u8]) -> Fastrie<'_, 'd, ()> {
+  Fastrie {
+    data,
+    index_width,
+    values: None,
+  }
 }
 
 impl<V> Fastrie<'_, '_, V> {
@@ -193,7 +210,7 @@ impl<V> Fastrie<'_, '_, V> {
     /// ```
     /// use fastrie::*;
     ///
-    /// let mut builder = FastrieBuilderNode::new();
+    /// let mut builder = FastrieBuilderNode::new(IndexWidth(1));
     /// builder.add(b"hell", 1);
     /// builder.add(b"hello", 2);
     /// builder.add(b"world", 4);
@@ -201,7 +218,7 @@ impl<V> Fastrie<'_, '_, V> {
     ///
     /// // `build.data` can be written as bytes to a file, or embedded directly into code as a literal byte array/slice.
     ///
-    /// let trie = Fastrie::from_prebuilt(&build.values, &build.data);
+    /// let trie = Fastrie::from_prebuilt(build.index_width, &build.values, &build.data);
     /// assert!(trie.contains_key(b"hello"));
     /// let query = b"hello world!";
     /// let mat = trie.longest_matching_prefix(query).unwrap();
@@ -214,8 +231,12 @@ impl<V> Fastrie<'_, '_, V> {
     /// assert_eq!(&query[..=mat.end], b"hell");
     /// assert_eq!(mat.value, &1);
     /// ```
-    pub const fn from_prebuilt<'v, 'd>(values: &'v [V], data: &'d [u8]) -> Fastrie<'v, 'd, V> {
-        Fastrie { values: Some(values), data }
+    pub const fn from_prebuilt<'v, 'd>(index_width: IndexWidth, values: &'v [V], data: &'d [u8]) -> Fastrie<'v, 'd, V> {
+        Fastrie {
+          data,
+          index_width,
+          values: Some(values),
+        }
     }
 
     pub fn memory_size(&self) -> usize {
@@ -226,19 +247,20 @@ impl<V> Fastrie<'_, '_, V> {
       let mut node_pos: usize = 0;
       let mut match_opt: Option<(usize, usize)> = None;
       'outer: for (i, &c) in text.iter().enumerate() {
-          if self.data[node_pos + IDX_BYTES] == 0 {
+          let idx_bytes = self.index_width.0;
+          if self.data[node_pos + idx_bytes] == 0 {
               // This node has no children.
               break;
           };
 
-          let mut cluster_pos: usize = node_pos + IDX_BYTES + 1;
+          let mut cluster_pos: usize = node_pos + idx_bytes + 1;
           loop {
-              let next_cluster_pos = read_idx(&self.data, cluster_pos);
-              let cluster_min: u8 = self.data[cluster_pos + IDX_BYTES];
-              let cluster_max: u8 = self.data[cluster_pos + IDX_BYTES + 1];
+              let next_cluster_pos = self.index_width.read_idx(&self.data, cluster_pos);
+              let cluster_min: u8 = self.data[cluster_pos + idx_bytes];
+              let cluster_max: u8 = self.data[cluster_pos + idx_bytes + 1];
               if c >= cluster_min && c <= cluster_max {
                   // Character is in this cluster, but it might point to a gap.
-                  node_pos = read_idx(&self.data, cluster_pos + IDX_BYTES + 2 + ((c - cluster_min) as usize) * IDX_BYTES);
+                  node_pos = self.index_width.read_idx(&self.data, cluster_pos + idx_bytes + 2 + ((c - cluster_min) as usize) * idx_bytes);
                   if node_pos == 0 {
                       // Character is not a child, as child node index is zero which means it's a gap.
                       break 'outer;
@@ -254,7 +276,7 @@ impl<V> Fastrie<'_, '_, V> {
           };
 
           // Get value of child node.
-          let node_value_idx: usize = read_idx(&self.data, node_pos);
+          let node_value_idx: usize = self.index_width.read_idx(&self.data, node_pos);
           if node_value_idx != 0 {
               match_opt = Some((i, node_value_idx - 1));
           };
